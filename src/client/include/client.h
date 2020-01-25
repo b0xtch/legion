2a4c12ecf1da9221b416e58ef9cf752d1bd8a91e
joergenr <joergen_reyes@sfu.ca>
2020-01-24 19:18:00 -0800

    Add client.cpp and .h for functions about sending data to players.

---
+++ src/client/include/client.h
@@ -0,0 +1,11 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <string>
+#include <fstream>
+#include <iostream>
+#include "lib/networking/include/Server.h"
+
+std::string getHtmlForNewContents();
+
+#endif
