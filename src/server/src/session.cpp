8745600ee27c818c24801e7cebcb31767ef68873
Nicholas Vasiu <nvasiu@sfu.ca>
2020-01-24 18:20:58 -0800

    added session class

---
+++ src/server/src/session.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "game.h"
+#include "players.h"
+
+void startGame() {}
+void endGame() {}
+void advanceGameTurn() {}
+void declareWinner() {}
+
+int main(){}
