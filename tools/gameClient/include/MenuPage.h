9d3520e27863eb22104faf7000591e9f63466984
Adrian Joziak <ajoziak@sfu.ca>
2020-01-31 21:03:32 -0800

    Add forms to menu pages

--- tools/gameClient/include/MenuPage.h
+++ tools/gameClient/include/MenuPage.h
@@ -5,17 +5,22 @@
 #include <string>
 #include <map>
 #include <menu.h>
-
-// TODO: Add option for adding forms
+#include <form.h>
 
 class MenuPage {
 public:
     
     MenuPage(std::string menu_name,
+             std::vector<const char *> fields, 
              std::vector<const char *> options, 
              std::vector<void (*)(std::map<std::string, MenuPage *>, 
                                   MenuPage **current_page)> option_results);
-    ~MenuPage();
+
+    void cleanup();
+
+    std::vector<const char *> get_fields();
+    FORM* get_form();
+    bool has_form();
 
     std::vector<const char *> get_options();
     std::vector<void (*)(std::map<std::string, MenuPage *>, 
@@ -24,6 +29,10 @@ public:
     int get_current_option();
     int change_current_option_on_input();
     int get_options_size();
+
+    static WINDOW *main_window;
+    static WINDOW *form_window;
+    static WINDOW *menu_window;
 
 private:
     MenuPage();
@@ -35,12 +44,19 @@ private:
                          MenuPage **current_page)> option_results;
     std::vector<const char *> options;
 
+    std::vector<const char *> fields;
+    
+    FORM *form;
+    FIELD **field_list;
+    FIELD *current_form;
+    int current_field;
+    int field_size;
+
     MENU *menu;
     ITEM **item_list;
     ITEM *current_item;
     int current_option;
     int options_size;
-    
 };
 
 #endif
