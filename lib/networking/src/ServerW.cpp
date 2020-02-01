93f14d1403a7e59fe3204e057148434d852cbc45
Nicholas Vasiu <nvasiu@sfu.ca>
2020-01-31 21:23:38 -0800

    added missing function receive()

--- lib/networking/src/ServerW.cpp
+++ lib/networking/src/ServerW.cpp
@@ -1,8 +1,12 @@
-void update() {
+void ServerW::update() {
     parentServer.update();
 }
 
-void sendTo(std::vector<Connection> connections, std::string text) {
+void ServerW::send(const std::deque<Message>& messages) {
+    parentServer.send(messages);
+}
+
+void ServerW::sendTo(std::vector<Connection> connections, std::string text) {
 
     std::deque<Message> newMessages;
     for (auto client : connections) {
@@ -12,6 +16,10 @@ void sendTo(std::vector<Connection> connections, std::string text) {
 
 }
 
-void disconnect(Connection connection) {
+std::deque<Message> ServerW::receive() {
+    return parentServer.receive;
+}
+
+void ServerW::disconnect(Connection connection) {
     parentServer.disconnect(connection);
 }
