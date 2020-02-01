a93189a702879bf6347c30631beab301e9f2985d
Nicholas Vasiu <nvasiu@sfu.ca>
2020-01-31 21:16:47 -0800

    added function to send a message to muliple clients

--- lib/networking/include/ServerW.h
+++ lib/networking/include/ServerW.h
@@ -6,6 +6,11 @@
 #include <memory>
 #include <string>
 #include <unordered_map>
+#include "server.h"
+
+/**
+A wrapper class for Server, with functions intended to help with communicating with several clients in different session.
+**/
 
 Class ServerW : public Server {
 
@@ -13,8 +18,9 @@ public:
     ServerW (Server* server) : parentServer(server) { }
 
     void update();
-    void sendTo(const std::deque<Message>& messages); // Send to chosen client
-    void sendToAll(const std::deque<Message>& messages); // Send to all clients
+    void send(const std::deque<Message>& messages);
+    void sendTo(std::vector<Connection> connections, std::string text); // Send the same message to a list of clients
+    [[nodiscard]] std::deque<Message> receive();
     void disconnect(Connection connection);
 
 private:
--- lib/networking/src/ServerW.cpp
+++ lib/networking/src/ServerW.cpp
@@ -1,7 +1,17 @@
-void update() {}
+void update() {
+    parentServer.update();
+}
 
-void sendTo(const std::deque<Message>& messages) {}
+void sendTo(std::vector<Connection> connections, std::string text) {
 
-void sendToAll(const std::deque<Message>& messages) {}
+    std::deque<Message> newMessages;
+    for (auto client : connections) {
+        newMessages.push_back({client, text});
+    }
+    parentServer.send(newMessages);
 
-void disconnect(Connection connection) {}
+}
+
+void disconnect(Connection connection) {
+    parentServer.disconnect(connection);
+}
