13181ced8584eceb1479d5755d85b1a6a1dcb954
Nicholas Vasiu <nvasiu@sfu.ca>
2020-02-06 13:41:06 -0800

    Renamed ServerW to something more descriptive, SimpleServer

--- lib/networking/include/SimpleServer.h
+++ lib/networking/include/SimpleServer.h
@@ -1,5 +1,5 @@
-#ifndef NETWORKING_SERVERW_H
-#define NETWORKING_SERVERW_H
+#ifndef NETWORKING_SIMPLESERVER_H
+#define NETWORKING_SIMPLESERVER_H
 
 #include <deque>
 #include <functional>
@@ -12,10 +12,10 @@
 A wrapper class for Server, with functions intended to help with communicating with several clients in different session.
 **/
 
-Class ServerW : public Server {
+Class SimpleServer : public Server {
 
 public:
-    ServerW (Server* server) : parentServer(server) { }
+    SimpleServer (Server* server) : parentServer(server) { }
 
     void update();
     void send(const std::deque<Message>& messages);
@@ -26,7 +26,7 @@ public:
 
 private:
     Server parentServer;
-    std::deque<Message> messageQueue;
+    std::deque<Message> outgoingMessages;
 
 };
 
---
+++ lib/networking/src/SimpleServer.cpp
@@ -0,0 +1,27 @@
+void SimpleServer::update() {
+    parentServer.update();
+}
+
+void SimpleServer::send(const std::deque<Message>& messages) {
+    parentServer.send(messages);
+}
+
+void SimpleServer::queueMessage(std::vector<Connection> connections, std::string text) {
+
+    for (auto client : connections) {
+        outgoingMessages.push_back({client, text});
+    }
+
+}
+
+void SimpleServer::sendQueuedMessages() {
+    parentServer.send(messageQueue);
+}
+
+std::deque<Message> SimpleServer::receive() {
+    return parentServer.receive;
+}
+
+void SimpleServer::disconnect(Connection connection) {
+    parentServer.disconnect(connection);
+}
