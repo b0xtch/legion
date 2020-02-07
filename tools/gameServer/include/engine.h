22fef12c059cb6246600f3c3ba0483bfe39f37b3
Mahmoud <mshehata@sfu.ca>
2020-02-07 14:49:34 -0800

    #19 convert Engine -> namespace now that it has lots of other structures

--- tools/gameServer/include/engine.h
+++ tools/gameServer/include/engine.h
@@ -8,114 +8,118 @@
 
 using namespace std; 
 
-// The main work is to split the string into the three components 
-// "player", ".", and "count" and turn these into instructions: "player" => look up 
-// "player" key; "." => expect it to be an inner map/object; "count" => look up 
-// "count" key in inner map/object
+namespace Engine {
 
-// these enums are here for testing ill move them out once I am done
-enum SetupTypes{
-    Kind, SetupPrompt,
-    KindInteger, KindString, KindBoolean,
-    KindQuestionAnswer, KindMultipleChoice  
-};
+    // The main work is to split the string into the three components 
+    // "player", ".", and "count" and turn these into instructions: "player" => look up 
+    // "player" key; "." => expect it to be an inner map/object; "count" => look up 
+    // "count" key in inner map/object
 
-enum RuleTypes{
-    Foreach, Loop, InParallel, Parallelfor,
-    Switch, When, Extend, Reverse, Shuffle, 
-    Sort, Deal, Discard, Add, Timer, 
-    InputChoice, InputText, InputVote, 
-    Message, GlobalMessage, Scores
-};
+    // these enums are here for testing ill move them out once I am done
+    enum SetupTypes{
+        Kind, SetupPrompt,
+        KindInteger, KindString, KindBoolean,
+        KindQuestionAnswer, KindMultipleChoice  
+    };
 
-template <typename G> 
-struct GenType {
-    GenType() : map() {};
-    GenType(std::map<G, std::any> &param) : map(param) {};
-    GenType(const G &param) : value(param) {};
+    enum RuleTypes{
+        Foreach, Loop, InParallel, Parallelfor,
+        Switch, When, Extend, Reverse, Shuffle, 
+        Sort, Deal, Discard, Add, Timer, 
+        InputChoice, InputText, InputVote, 
+        Message, GlobalMessage, Scores
+    };
 
-    std::map<G, std::any> map;
-    G value;
-};
+    template <typename G> 
+    struct GenType {
+        GenType() : map() {};
+        GenType(std::map<G, std::any> &param) : map(param) {};
+        GenType(const G &param) : value(param) {};
 
-// {
-//   "kind": <<data kind>>,
-//   "prompt": <<Description of what data the owner should provide>>
-// }
-struct Setup: GenType<std::pair<std::string, std::any>> {
-    Setup(const std::pair<std::string, std::any> &param) // change the any to json
-        : GenType(param) {}; 
+        std::map<G, std::any> map;
+        G value;
+    };
 
-    // overload the cout to be able to cout a custom thing like the prompt for example
-};
+    // {
+    //   "kind": <<data kind>>,
+    //   "prompt": <<Description of what data the owner should provide>>
+    // }
+    struct Setup: GenType<std::pair<std::string, std::any>> {
+        Setup(const std::pair<std::string, std::any> &param) // change the any to json
+            : GenType(param) {}; 
 
-/**
- * Convert my interpreter into self executing lambda functions
- * from this cool article on dev
- * still grasping how to use variants for each component of our game
- * 
- * https://dev.to/tmr232/that-overloaded-trick-overloading-lambdas-in-c17
-*/
+        // overload the cout to be able to cout a custom thing like the prompt for example
+    };
 
-template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
-template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>; 
+    /**
+     * Convert my interpreter into self executing lambda functions
+     * from this cool article on dev
+     * still grasping how to use variants for each component of our game
+     * 
+     * https://dev.to/tmr232/that-overloaded-trick-overloading-lambdas-in-c17
+    */
 
-template<typename... T>
-struct Components{
-    using component = std::variant<T...>;
+    template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+    template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>; 
 
-    // These are the main types that we are going to 
-    // have and translate to from type T
-    // JsonDSL::SpecificationFields;
-    // JsonDSL::ConfigFields;
-    // JsonDSL::RuleType;
-    // JsonDSL::RuleParameters;
-    // JsonDSL::TimerModes;
-    // JsonDSL::SetupFields;
+    template<typename... T>
+    struct Components{
+        using component = std::variant<T...>;
 
-    template<typename V>
-    void visit(V&& visitor){
-        for (auto& entity : entities){
-            std::visit(overloaded {
-                [](int& _in){_in += _in;},
-                [](double& _in){_in += _in;},
-                [](std::string& _in){_in += _in;},
-                [](bool& ){std::cout << "bool item" << std::endl;},
-                [](JsonDSL::RuleType value){std::cout << "rules item" << " " << RuleTypes(value) << std::endl;},
-                [](JsonDSL::SetupFields value){
-                    std::cout << "Upload your json!" << std::endl;
+        // These are the main types that we are going to 
+        // have and translate to from type T
+        // JsonDSL::SpecificationFields;
+        // JsonDSL::ConfigFields;
+        // JsonDSL::RuleType;
+        // JsonDSL::RuleParameters;
+        // JsonDSL::TimerModes;
+        // JsonDSL::SetupFields;
 
-                    //Setup( /* map[ make_pair("kind", json ] = "the prompt"; */ );
-                }
-                // .. add th rest once I have clear understanding of whats happening
-            }, entity);
+        template<typename V>
+        void visit(V&& visitor){
+            for (auto& entity : entities){
+                std::visit(overloaded {
+                    [](int& _in){_in += _in;},
+                    [](double& _in){_in += _in;},
+                    [](std::string& _in){_in += _in;},
+                    [](bool& ){std::cout << "bool item" << std::endl;},
+                    [](JsonDSL::RuleType value){std::cout << "rules item" << " " << RuleTypes(value) << std::endl;},
+                    [](JsonDSL::SetupFields value){
+                        std::cout << "Upload your json!" << std::endl;
+
+                        //Setup( /* map[ make_pair("kind", json ] = "the prompt"; */ );
+                    }
+                    // .. add th rest once I have clear understanding of whats happening
+                }, entity);
+            }
         }
-    }
 
-    // Create a vector of variants
-    std::vector<component> entities;
-};
+        // Create a vector of variants
+        std::vector<component> entities;
+    };
 
 
-template <typename T> 
-class Engine{ 
-    public:
-        Engine (T input);
+    template <typename T> 
+    class EngineImpl{ 
+        public:
+            Engine (T input);
 
-        bool validGameConfig(T input);
-        GenType<T> getGameConfig();
+            bool validGameConfig(T input);
+            GenType<T> getGameConfig();
 
-    private:
-        T input;
-        GenType<T> gameConfig;
+        private:
+            T input;
+            GenType<T> gameConfig;
 
-        void initalizeEngine();
-        void buildGame();
-        void mapKeyToValue(T key, T value);
-        T mapValueToFuntion(T value);
+            void initalizeEngine();
+            void buildGame();
+            void mapKeyToValue(T key, T value);
+            T mapValueToFuntion(T value);
 
-        // Control Structure Methods
-        void findAndExecute(/* find a specific function and execute dynamically*/);
-};
+            // Control Structure Methods
+            void findAndExecute(/* find a specific function and execute dynamically*/);
+    };
+
+}
 
 #endif
