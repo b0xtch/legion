5b050b03258cd46eacfc1d0dbddd86eb58719031
qlnguyen <quoc_luong_nguyen@sfu.ca>
2020-02-07 22:19:03 -0800

    fixed conflicts

--- tools/gameServer/include/session.h
+++ tools/gameServer/include/session.h
@@ -4,35 +4,8 @@
 #include <vector>
 #include <unordered_set>
 
-// class Game;
 class Session {
-  // friend class Game;
 public:
-<<<<<<< HEAD
-    Session(int ID, Game& game, Player& sessionCreator);
-    ~Session();
-
-    void updateTime();
-    void checkTimeOut();
-
-    void startGame();	
-	void endGame();
-    void advanceGameTurn(); //state machine???
-    void declareWinner();
-
-    void addPlayer(Player& newPlayer);
-    void removePlayer(int removePlayerID);
-
-private:
-    Game sessionGame; //each session should only handle 1 Game at a time for now
-    std::vector<Player> sessionPlayers;
-
-    int sessionID;
-    double maxIdleTime = 30;
-    std::chrono::system_clock::time_point lastUpdateTime;
-
-};
-=======
     Session();
     std::vector<networking::Connection> getAllClients();
     networking::Connection getClient(uintptr_t id);
@@ -45,6 +18,5 @@ private:
     std::string sessionId;
     int sessionClientLimit;
 }
->>>>>>> 043e60730ab5c04021be8f0f471e821e25ee1540
 
 #endif
