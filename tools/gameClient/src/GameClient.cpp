857feef5e91cc6357ae1b2a16ff0082390e58507
Nicholas Vasiu <nvasiu@sfu.ca>
2020-02-11 21:29:16 -0800

    added gameClient method to turn user input into json to send to server

--- tools/gameClient/src/GameClient.cpp
+++ tools/gameClient/src/GameClient.cpp
@@ -6,6 +6,8 @@
 #include "../include/MenuPage.h"
 #include "../../../lib/networking/include/Client.h"
 
+using namespace std;
+
 // Basic menu structure referenced from: 
 //    http://tldp.org/HOWTO/NCURSES-Programming-HOWTO/menus.html
 
@@ -23,6 +25,8 @@ void switch_to_join_lobby_page();
 void switch_to_create_lobby_page();
 void exit_program();
 
+// Make json message for server
+string makeServerMessage(string input);
 
 int main(int argc, char* argv[]) {
 
@@ -40,7 +44,7 @@ int main(int argc, char* argv[]) {
     const MenuPage::FunctionList connect_menu_item_results
         = {connect_to_server};
 
-    std::string connect_menu_name = "Connect menu";
+    string connect_menu_name = "Connect menu";
     MenuPage *connect_menu_page = new MenuPage( connect_menu_name,
                                                connect_menu_fields,
                                                connect_menu_items, 
@@ -57,7 +61,7 @@ int main(int argc, char* argv[]) {
     const MenuPage::FunctionList main_menu_item_results
         = {switch_to_join_lobby_page, switch_to_create_lobby_page, exit_program};
 
-    std::string main_menu_name = "Main menu";
+    string main_menu_name = "Main menu";
     MenuPage *main_menu_page = new MenuPage( main_menu_name,
                                             main_menu_fields, 
                                             main_menu_items, 
@@ -75,7 +79,7 @@ int main(int argc, char* argv[]) {
     const MenuPage::FunctionList join_lobby_item_results
         = {switch_to_main_menu_page, switch_to_main_menu_page};
 
-    std::string join_lobby_name = "Join lobby";
+    string join_lobby_name = "Join lobby";
     MenuPage *join_lobby_page = new MenuPage( join_lobby_name,
                                              join_lobby_fields,
                                              join_lobby_items, 
@@ -94,7 +98,7 @@ int main(int argc, char* argv[]) {
            switch_to_main_menu_page, switch_to_main_menu_page
            }; 
 
-    std::string create_lobby_name = "Create lobby";
+    string create_lobby_name = "Create lobby";
     MenuPage *create_lobby_page = new MenuPage( create_lobby_name,
                                                create_lobby_fields, 
                                                create_lobby_items, 
@@ -109,6 +113,7 @@ int main(int argc, char* argv[]) {
 
     MenuManager::cleanup();
 
+    // ----------------------------------------------------
     // main loop
 
     if (argc < 3) {
@@ -116,9 +121,14 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    networking::Client client{argv[1], argv[2]};
+
     bool done = false;
 
-    // Get user input and call SendServerMessage(input)
+    string userInput;
+    // Menu gets user input and sets the above variable
+    client.send( makeServerMessage(userInput) );
+
     while (!done && !client.isDisconnected()) {
         try {
             client.update();
@@ -132,7 +142,6 @@ int main(int argc, char* argv[]) {
             // Give user response
         }
 
-        // Update chat window
     }
 
     return 0;
@@ -141,7 +150,7 @@ int main(int argc, char* argv[]) {
 void connect_to_server() {
     // TODO: connect to server with given ip and port in fields
     MenuPage *connect_page = MenuManager::get_current_page();
-    std::vector<FIELD *> connect_fields = connect_page->get_field_list();
+    vector<FIELD *> connect_fields = connect_page->get_field_list();
 
     const int server_ip_input_index = 1;
     char *server_ip = field_buffer( connect_fields[server_ip_input_index], 0 );
@@ -154,17 +163,17 @@ void connect_to_server() {
 }
 
 void switch_to_join_lobby_page() {
-    std::string next_page_name = "Join lobby";
+    string next_page_name = "Join lobby";
     MenuManager::switch_page( next_page_name );
 }
 
 void switch_to_create_lobby_page() {
-    std::string next_page_name = "Create lobby";
+    string next_page_name = "Create lobby";
     MenuManager::switch_page( next_page_name );
 }
 
 void switch_to_main_menu_page() {
-    std::string next_page_name = "Main menu";
+    string next_page_name = "Main menu";
     MenuManager::switch_page( next_page_name );
 }
 
@@ -172,7 +181,39 @@ void exit_program() {
     
 }
 
-void sendServerMessage(std::string input) {
+/** 
+Takes a string input and converts it into a json object consisting of 2 fields.
+The first field is the command to be given to the server. The second field is any extra arguments for the command.
+
+If the first word of the input matches a possible command, it is set as the value for the command field.
+Then the rest of the input is put in the data field.
+
+If the first word does not match a possible command, the input is assumed to be a chat message.
+The command is set to !chat, and the entire input is put in the data field.
+**/
+string makeServerMessage(string input) {
     
+    vector<string> possibleCommands;
+    possibleCommands.push_back("!createsession");
+    possibleCommands.push_back("!joinsession");
+
+    string command = "{ \"command\": \"";
+    
+    size_t endOfCommand = input.find(" ");
+    string firstWord = input.substr(0,endOfCommand);
+    if (firstWord == "!example") {
+        command += firstWord;
+    } else {
+        command += "!chat";
+        endOfCommand = 0;
+    }
+
+    string data = "\", \"data\": \"";
+    data += input.substr(endOfCommand, string::npos);
+
+    auto message = json::parse( command+data+"\" }" );
+
+    return message.dump();
+
 }
 
