7f0b90c155aa22055e59d075950a95251679877a
kca123 <kca123@sfu.ca>
2020-03-06 01:58:43 -0800

    finish definition for validateSingleRule

--- lib/json/src/ruleValidator.cpp
+++ lib/json/src/ruleValidator.cpp
@@ -18,7 +18,12 @@ static void validateAllParametersAreValid(const json& ruleJson, const Rule& rule
 
 }
 
+//function declaration for validateSingleRule to use as both functions
+//call each other in a recursive fashion
+static void validateRulesStructure(const json& rulesJson);
+
 static void validateSingleRule(const json& ruleJson){
+    //check that a rule key is present in the json and has a valid value
     std::string ruleString = dsl.getRuleParameterString(JsonDSL::Rule);
     
     if(!ruleJson.contains(ruleString)){
@@ -26,6 +31,28 @@ static void validateSingleRule(const json& ruleJson){
     }
 
     std::string ruleName = ruleJson[ruleString];
+    auto ruleIterator = ruleMap.find(ruleName);
+
+    if(ruleIterator == ruleMap.end()){
+        throw std::invalid_argument(ruleName.append(" is not a valid rule."));
+    }
+
+    //get the rule object from the iterator and then use the other validate methods
+    //to ensure the other data in the rule is valid
+    auto ruleDefiniton = (*ruleIterator).second;
+    validateNecessaryParametersPresent(ruleJson, ruleDefiniton);
+    validateAllParametersAreValid(ruleJson, ruleDefiniton);
+
+    if(ruleDefiniton.hasCases){
+        json cases = ruleJson[dsl.getRuleParameterString(JsonDSL::Cases)];
+        for(auto caseJson : cases){
+            auto nestedRules = caseJson[dsl.getSpecString(JsonDSL::Rules)];
+            validateRulesStructure(nestedRules);
+        }
+    } else if (ruleDefiniton.hasSetOfRules){
+        json nestedRules = ruleJson[dsl.getSpecString(JsonDSL::Rules)];
+        validateRulesStructure(nestedRules);
+    }
 }
 
 static void validateRulesStructure(const json& rulesJson){
