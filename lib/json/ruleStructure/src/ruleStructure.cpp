f77a1e80ca4c691e5e898cf46dc11e50e7885c67
kca123 <kca123@sfu.ca>
2020-03-12 01:06:24 -0700

    add other rule types to ruleStructure

--- lib/json/ruleStructure/src/ruleStructure.cpp
+++ lib/json/ruleStructure/src/ruleStructure.cpp
@@ -49,10 +49,22 @@ vecIterator Rule::getParametersEnd(){
 RuleMap ruleValidationHelper::getRuleMap(){
     bool containsOtherRules = true;
 
-    ruleValidationHelper::Rule Add = {
-        JsonDSL::Add,
-        {JsonDSL::To, JsonDSL::Value},
-        !containsOtherRules
+    ruleValidationHelper::Rule Foreach = {
+        JsonDSL::Foreach,
+        {JsonDSL::List, JsonDSL::Element},
+        containsOtherRules
+    };
+
+    ruleValidationHelper::Rule Loop = {
+        JsonDSL::Loop,
+        {JsonDSL::Until, JsonDSL::While},
+        containsOtherRules
+    };
+
+    ruleValidationHelper::Rule Inparallel = {
+        JsonDSL::InParallel,
+        {},
+        containsOtherRules
     };
 
     ruleValidationHelper::Rule Parallelfor = {
@@ -61,8 +73,122 @@ RuleMap ruleValidationHelper::getRuleMap(){
         containsOtherRules
     };
 
+    ruleValidationHelper::Rule Switch = {
+        JsonDSL::Switch,
+        {JsonDSL::Value, JsonDSL::List, JsonDSL::Cases, JsonDSL::Case},
+        containsOtherRules
+    };
+
+    ruleValidationHelper::Rule When = {
+        JsonDSL::When,
+        {JsonDSL::Cases, JsonDSL::Condition},
+        containsOtherRules
+    };
+
+    ruleValidationHelper::Rule Extend = {
+        JsonDSL::Extend,
+        {JsonDSL::Target, JsonDSL::List},
+        !containsOtherRules
+    };
+
+    ruleValidationHelper::Rule Reverse = {
+        JsonDSL::Reverse,
+        {JsonDSL::List},
+        !containsOtherRules
+    };
+
+    ruleValidationHelper::Rule Shuffle = {
+        JsonDSL::Shuffle,
+        {JsonDSL::List},
+        !containsOtherRules
+    };
+
+    ruleValidationHelper::Rule Sort = {
+        JsonDSL::Sort,
+        {JsonDSL::List, JsonDSL::Key},
+        !containsOtherRules
+    };
+
+    ruleValidationHelper::Rule Deal = {
+        JsonDSL::Deal,
+        {JsonDSL::From, JsonDSL::To, JsonDSL::Count},
+        !containsOtherRules
+    };
+
+    ruleValidationHelper::Rule Discard = {
+        JsonDSL::Discard,
+        {JsonDSL::From, JsonDSL::To, JsonDSL::Count},
+        !containsOtherRules
+    };
+
+    ruleValidationHelper::Rule Add = {
+        JsonDSL::Add,
+        {JsonDSL::To, JsonDSL::Value},
+        !containsOtherRules
+    };
+
+    ruleValidationHelper::Rule Timer = {
+        JsonDSL::Timer,
+        {JsonDSL::Duration, JsonDSL::Mode, JsonDSL::Flag},
+        containsOtherRules
+    };
+
+    ruleValidationHelper::Rule InputChoice = {
+        JsonDSL::InputChoice,
+        {JsonDSL::To, JsonDSL::RulePrompt, JsonDSL::Choices, JsonDSL::Result, JsonDSL::Timeout},
+        !containsOtherRules
+    };
+
+    ruleValidationHelper::Rule InputText = {
+        JsonDSL::InputText,
+        {JsonDSL::To, JsonDSL::RulePrompt, JsonDSL::Result, JsonDSL::Timeout},
+        !containsOtherRules
+    };
+
+    ruleValidationHelper::Rule InputVote = {
+        JsonDSL::InputVote,
+        {JsonDSL::To, JsonDSL::RulePrompt, JsonDSL::Choices, JsonDSL::Result, JsonDSL::Timeout},
+        !containsOtherRules
+    };
+
+    ruleValidationHelper::Rule Message = {
+        JsonDSL::Message,
+        {JsonDSL::To, JsonDSL::Value},
+        !containsOtherRules
+    };
+
+    ruleValidationHelper::Rule GlobalMessage = {
+        JsonDSL::GlobalMessage,
+        {JsonDSL::Value},
+        containsOtherRules
+    };
+
+    ruleValidationHelper::Rule Score = {
+        JsonDSL::Scores,
+        {JsonDSL::Score, JsonDSL::Ascending},
+        !containsOtherRules
+    };
+
     RuleMap map;
-    map.insert(std::make_pair(dsl.getRuleString(Add.name), Add));
+    map.insert(std::make_pair(dsl.getRuleString(Foreach.name), Foreach));
+    map.insert(std::make_pair(dsl.getRuleString(Loop.name), Loop));
+    map.insert(std::make_pair(dsl.getRuleString(Inparallel.name), Inparallel));
     map.insert(std::make_pair(dsl.getRuleString(Parallelfor.name), Parallelfor));
+    map.insert(std::make_pair(dsl.getRuleString(Switch.name), Switch));
+    map.insert(std::make_pair(dsl.getRuleString(When.name), When));
+    map.insert(std::make_pair(dsl.getRuleString(Extend.name), Extend));
+    map.insert(std::make_pair(dsl.getRuleString(Reverse.name), Reverse));
+    map.insert(std::make_pair(dsl.getRuleString(Shuffle.name), Shuffle));
+    map.insert(std::make_pair(dsl.getRuleString(Sort.name), Sort));
+    map.insert(std::make_pair(dsl.getRuleString(Deal.name), Deal));
+    map.insert(std::make_pair(dsl.getRuleString(Discard.name), Discard));
+    map.insert(std::make_pair(dsl.getRuleString(Add.name), Add));
+    map.insert(std::make_pair(dsl.getRuleString(Timer.name), Timer));
+    map.insert(std::make_pair(dsl.getRuleString(InputChoice.name), InputChoice));
+    map.insert(std::make_pair(dsl.getRuleString(InputText.name), InputText));
+    map.insert(std::make_pair(dsl.getRuleString(InputVote.name), InputVote));
+    map.insert(std::make_pair(dsl.getRuleString(Message.name), Message));
+    map.insert(std::make_pair(dsl.getRuleString(GlobalMessage.name), GlobalMessage));
+    map.insert(std::make_pair(dsl.getRuleString(Score.name), Score));
     return map;
 }
