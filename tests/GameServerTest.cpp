35c897dc7de9d7efd9404482d0cbc08177b32646
joergenr <joergen_reyes@sfu.ca>
2020-03-18 19:32:20 -0700

    Rename test files and add WIP GameServerTest file

---
+++ tests/GameServerTest.cpp
@@ -0,0 +1,12 @@
+#include <chrono>
+#include <thread>
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "GameServer.h"
+#include "Server.h"
+#include "Client.h"
+
+TEST(GameServerTests, constructor) {
+    
+}
