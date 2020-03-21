b227ca69cf853ff7c0dd0d043ac058af9efcadbc
kca123 <kca123@sfu.ca>
2020-03-21 03:09:46 -0700

    add function to check if a string expression is valid bool

--- lib/json/src/variableValidator.cpp
+++ lib/json/src/variableValidator.cpp
@@ -2,12 +2,41 @@
 #include "jsonDSL.h"
 #include <vector>
 #include <string>
+#include "../abseil-cpp/absl/strings/str_split.h"
+#include <boost/algorithm/string.hpp>
 
 using json = nlohmann::json;
 
 static JsonDSL dsl;
 
-JsonDSL::VariableDataType checkForDataType(const nlohmann::json& j_object){
+static std::vector<std::string> intFuncs = {"upfrom"};
+static std::vector<std::string> listFuncs = {"collect", "contains", "elements"};
+
+std::optional<JsonDSL::VariableDataType> getLiteralTypeFromString(std::string expression, varMap& map){
+    auto boolVars = map[JsonDSL::VarBoolean];
+    auto strVars = map[JsonDSL::VarString];
+    auto intVars = map[JsonDSL::VarInteger];
+
+    auto boolIt = std::find(boolVars.begin(), boolVars.end(), expression);
+    auto strIt = std::find(strVars.begin(), strVars.end(), expression);
+    auto intIt = std::find(intVars.begin(), intVars.end(), expression);
+
+    bool boolFound = boolIt != boolVars.end();
+    bool strFound = strIt != strVars.end();
+    bool intFound = intIt != intVars.end();
+
+    if(boolFound){
+        return JsonDSL::VarBoolean;
+    }else if(strFound){
+        return JsonDSL::VarString;
+    }else if(intFound){
+        return JsonDSL::VarInteger;
+    }else {
+        return {};
+    }
+}
+
+JsonDSL::VariableDataType getTypeFromJson(const nlohmann::json& j_object){
     if(j_object.is_number_integer()){
         return JsonDSL::VarInteger;
     }else if (j_object.is_boolean()){
@@ -36,16 +65,16 @@ using varMap = std::map<JsonDSL::VariableDataType, varCollection>;
 
 static void collectLiteralVarsWithPrepend(json& j_object, const std::string& prependStr, varMap& map){
     for(auto jsonItem : j_object.items()){
-        JsonDSL::VariableDataType varType = checkForDataType(jsonItem);
+        JsonDSL::VariableDataType varType = getTypeFromJson(jsonItem);
 
         if(!isLiteralVar(varType)){
             continue;
         }
 
-        std::stringstream ss;
-        ss << prependStr << jsonItem.key();
+        std::stringstream varName;
+        varName << prependStr << jsonItem.key();
 
-        map[varType].push_back(ss.str());
+        map[varType].push_back(varName.str());
         j_object.erase(jsonItem.key());
     }
 }
@@ -63,6 +92,45 @@ static varMap getEmptyVarMap(){
     map.insert(std::make_pair(JsonDSL::VarObject, varCollection{}));
     return map;
 }
+
+static void collectListAndObjects(json& j_object, varMap& map){
+
+}
+
+bool isBooleanExpression(std::string expression, varMap& map){
+    varCollection& bools = map[JsonDSL::VarBoolean];
+    auto boolIt = std::find(bools.begin(), bools.end(), expression);
+    if(boolIt != bools.end()){
+        return true;
+    }
+
+    std::string compareOp = "==";
+    auto compareLoc = expression.find(compareOp);
+    
+    if(compareLoc == std::string::npos){
+        return false;
+    }
+
+    std::vector<std::string> args = absl::StrSplit(expression, compareOp);
+
+    //assuming that boolean expressions do not include && or || as it's not listed in spec
+    if(args.size() != 2){
+        return false;
+    }
+
+    boost::trim(args.at(0));
+    boost::trim(args.at(1));
+
+    auto firstVarType = getLiteralTypeFromString(args.at(0), map);
+    auto secondVarType = getLiteralTypeFromString(args.at(1), map);
+
+    if(!firstVarType.has_value || !secondVarType.has_value){
+        return false;
+    }
+    
+    return firstVarType.value == secondVarType.value;
+}
+
 
 static varMap collectAllVariables(const json& j_object){
     std::string configStr = dsl.getSpecString(JsonDSL::Configuration);
@@ -86,14 +154,13 @@ static varMap collectAllVariables(const json& j_object){
     collectLiteralVars(constVars, map);
     collectLiteralVars(varVars, map);
     
-
     return map;
 }
 
 void VariableValidator::validateVariableUsage(const json& j_object){
     std::string rulesStr = dsl.getSpecString(JsonDSL::Rules);
     json rules = j_object[rulesStr];
-    collectAllVariables(j_object);
+    varMap map = collectAllVariables(j_object);
 }
 
 VariableValidator::VariableValidator(){}
