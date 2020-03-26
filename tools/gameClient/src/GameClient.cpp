6a4485816798a0e4dbf7a73b331c104802be633d
joergenr <joergen_reyes@sfu.ca>
2020-03-25 18:10:12 -0700

    Change create lobby page to show game list in chat and require manual entry of games
    Also continued refactoring of menu init code. Warning that this will crash if you create lobby then do anything else. But the server does get the create lobby message.

--- tools/gameClient/src/GameClient.cpp
+++ tools/gameClient/src/GameClient.cpp
@@ -189,7 +189,7 @@ std::string getGamesList() {
 
 }
 
-void initializePages() {
+void initializePages(MenuManager &menuManager, bool &done, networking::Client &client) {
     // Build page for creating lobby
     // Build page for joining lobby
     // Build page for main menu
@@ -204,7 +204,6 @@ MenuPageInfo::MenuName buildJoinLobbyPage(MenuManager &menuManager, networking::
     const MenuPageInfo::NameList joinLobbyItems = {"Join", "Back"};
 
     // Join lobby item functions
-
     auto moveBackToMainMenuPage = [&menuManager] () {
         MenuPageInfo::MenuName nextPage = "Main menu";
         menuManager.switchPage( nextPage );
@@ -231,14 +230,83 @@ MenuPageInfo::MenuName buildJoinLobbyPage(MenuManager &menuManager, networking::
         = {joinLobby, moveBackToMainMenuPage};
 
     MenuPageInfo::MenuName joinLobbyName = "Join lobby";
-    std::shared_ptr<MenuPageInfo> joinLobbyPage = std::make_shared<MenuPageInfo>(
-                                              joinLobbyName,
-                                              joinLobbyFields,
-                                              joinLobbyItems,
-                                              joinLobbyItemResults );
+    auto joinLobbyPage = std::make_shared<MenuPageInfo>(joinLobbyName, joinLobbyFields, joinLobbyItems, joinLobbyItemResults );
     menuManager.addPage( joinLobbyPage );
     
     return "Join lobby";
+}
+
+std::string requestGames(networking::Client& client) {
+    std::string serverMessage = ParsedMessage::makeMsgText(PMConstants::TYPE_LIST_GAMES, "");
+    client.send(serverMessage);
+    
+    client.update();
+    auto response = client.receive();
+    while ( response.empty() ) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        client.update();
+        response = client.receive();
+    }
+    
+    ParsedMessage pm = ParsedMessage::interpret(response);
+    
+    if (pm.getType() == ParsedMessage::Type::ListGames) {
+        return pm.getData();
+    }
+    else {
+        return "<no games>";
+    }
+}
+
+void displayGames(MenuManager& menuManager, const std::string& games) {
+    std::vector<std::string> gamesList = splitString(games, "\n", false);
+    std::stringstream ss{};
+    ss << "--- GAME TITLES\n";
+    for (auto& game : gamesList) {
+        ss << game << "\n";
+    }
+    ss << "---\n";
+    
+    menuManager.displayChatText(ss.str());
+}
+
+MenuPageInfo::MenuName buildCreateLobbyPage(MenuManager &menuManager, networking::Client& client) {
+    
+    const MenuPageInfo::NameList createLobbyFields = {"Game title:"};
+
+    const MenuPageInfo::NameList createLobbyItems = {"Create", "Back"};
+
+    // Join lobby item functions
+    auto moveBackToMainMenuPage = [&menuManager] () {
+        MenuPageInfo::MenuName nextPage = "Main menu";
+        menuManager.switchPage( nextPage );
+    };
+
+    auto createLobby = [&client, &menuManager] () {
+        std::shared_ptr<MenuPage> createPage = menuManager.getCurrentPage();
+        form_driver(createPage->getForm(), REQ_VALIDATION);
+        
+        MenuPage::FieldList *connectFields = createPage->getFieldList();
+
+        const int gameTitleInputIndex = 1;
+        const char *gameTitle =
+            field_buffer( connectFields->at( gameTitleInputIndex ), 0 );
+        std::string gameTitleString(gameTitle);
+        // Bug: if field is not filled to max length, only spaces will be sent.
+        // What's happening: https://alan-mushi.github.io/2014/11/30/ncurses-forms.html
+        // Fix: https://stackoverflow.com/questions/18493449/how-to-read-an-incomplete-form-field-ncurses-c
+        std::string serverMessage = ParsedMessage::makeMsgText(PMConstants::TYPE_CREATE_SESSION, gameTitleString);
+        client.send( serverMessage );
+    };
+
+    const MenuPageInfo::FunctionList createLobbyItemResults
+        = {createLobby, moveBackToMainMenuPage};
+
+    MenuPageInfo::MenuName createLobbyName = "Create lobby";
+    auto createLobbyPage = std::make_shared<MenuPageInfo>(createLobbyName, createLobbyFields, createLobbyItems, createLobbyItemResults );
+    menuManager.addPage( createLobbyPage );
+    
+    return "Create lobby";
 }
 
 void initializeMenuPages( MenuManager &menuManager, bool &done,
@@ -250,6 +318,7 @@ void initializeMenuPages( MenuManager &menuManager, bool &done,
     MenuPageInfo::NameList mainMenuItems = { "Join lobby", "Create lobby", "Exit" };
 
     MenuPageInfo::MenuName joinLobbyMenuName = buildJoinLobbyPage(menuManager, client);
+    MenuPageInfo::MenuName createLobbyMenuName = buildCreateLobbyPage(menuManager, client);
 
     // Main menu item functions
     auto moveToJoinLobbyPage = [&menuManager, joinLobbyMenuName] () {
@@ -257,67 +326,11 @@ void initializeMenuPages( MenuManager &menuManager, bool &done,
         menuManager.switchPage( nextPage );
     };
     
-    auto moveToCreateLobbyPage = [&client, &menuManager] () {
-
-        // Get the list of games
-        std::string serverMessage = ParsedMessage::makeMsgText(PMConstants::TYPE_LIST_GAMES, "");
-        client.send(serverMessage);
-
-        client.update();
-        auto response = client.receive();
-        while ( response.empty() ) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
-            client.update();
-            response = client.receive();
-        }
-
-        MenuPageInfo::NameList gamesList = {"Game 1", "Game 2"};
-            /* TEMP create list from response */
-
-        // Define the function that is called when a game is chosen
-
-        auto chooseGame = [&client, &gamesList, &menuManager] () {
-
-            //std::string gameChoice = gamesList[menuManager.getSelectedIndex()];
-            //client.send( gameChoice );
-
-        };
-
-        // Create the "Creat lobby" menu page with the given games
-
-        MenuPageInfo::NameList createLobbyFields = {};
-
-        MenuPageInfo::NameList createLobbyItems = {};
-
-        for ( auto gameName : gamesList ) {
-            createLobbyItems.push_back( gameName );
-        }
-        MenuPageInfo::ItemName backString = "back";
-        createLobbyItems.push_back( backString );
-
-        MenuPageInfo::FunctionList createLobbyItemResults = {};
-
-        for ( int i = 0; i < gamesList.size(); i++ ) {
-            createLobbyItemResults.push_back( chooseGame );
-        }
-
-        auto moveBackToMainMenuPage = [&menuManager] () {
-            MenuPageInfo::MenuName nextPage = "Main menu";
-            menuManager.switchPage( nextPage );
-        };
-        createLobbyItemResults.push_back( moveBackToMainMenuPage );
-
-        MenuPageInfo::MenuName createLobbyName = "Create lobby";
-        std::shared_ptr<MenuPageInfo> createLobbyPage = std::make_shared<MenuPageInfo>(
-                                                    createLobbyName,
-                                                    createLobbyFields,
-                                                    createLobbyItems,
-                                                    createLobbyItemResults );
-        menuManager.addPage( createLobbyPage );
-
-        // The games are all loaded and the page is setup, so switch to the page
-
-        MenuPageInfo::MenuName nextPage = "Create lobby";
+    auto moveToCreateLobbyPage = [&menuManager, &client, createLobbyMenuName] () {
+        std::string response = requestGames(client);
+        displayGames(menuManager, response);
+        
+        MenuPageInfo::MenuName nextPage = createLobbyMenuName;
         menuManager.switchPage( nextPage );
     };
 
@@ -336,48 +349,6 @@ void initializeMenuPages( MenuManager &menuManager, bool &done,
                                              mainMenuItems,
                                              mainMenuItemResults );
     menuManager.addPage( mainMenuPage );
-
-
-    /*
-    // Join lobby menu
-    const MenuPageInfo::NameList joinLobbyFields = {"Lobby code:"};
-
-    const MenuPageInfo::NameList joinLobbyItems = {"Join", "Back"};
-
-    // Join lobby item functions
-
-    auto moveBackToMainMenuPage = [&menuManager] () {
-        MenuPageInfo::MenuName nextPage = "Main menu";
-        menuManager.switchPage( nextPage );
-    };
-
-    auto joinLobby = [&done, &client, &menuManager] () {
-        std::shared_ptr<MenuPage> joinPage = menuManager.getCurrentPage();
-        form_driver(joinPage->getForm(), REQ_VALIDATION);
-        
-        MenuPage::FieldList *connectFields = joinPage->getFieldList();
-
-        const int lobbyCodeInputIndex = 1;
-        const char *lobbyCode =
-            field_buffer( connectFields->at( lobbyCodeInputIndex ), 0 );
-        std::string lobbyCodeString(lobbyCode);
-        // Bug: if field is not filled to max length, only spaces will be sent.
-        // What's happening: https://alan-mushi.github.io/2014/11/30/ncurses-forms.html
-        // Fix: https://stackoverflow.com/questions/18493449/how-to-read-an-incomplete-form-field-ncurses-c
-        std::string serverMessage = ParsedMessage::makeMsgText(PMConstants::TYPE_JOIN_SESSION, lobbyCodeString);
-        client.send( serverMessage );
-    };
-
-    const MenuPageInfo::FunctionList joinLobbyItemResults
-        = {joinLobby, moveBackToMainMenuPage};
-
-    MenuPageInfo::MenuName joinLobbyName = "Join lobby";
-    std::shared_ptr<MenuPageInfo> joinLobbyPage = std::make_shared<MenuPageInfo>(
-                                              joinLobbyName,
-                                              joinLobbyFields,
-                                              joinLobbyItems,
-                                              joinLobbyItemResults );
-    menuManager.addPage( joinLobbyPage );*/
 
     menuManager.setCurrentPage( mainMenuPage->menuName );
 }
